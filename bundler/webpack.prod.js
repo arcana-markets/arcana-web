837ef3238ae3ff42f112c64ac6b35f0259c5bf70
kizmt <kizmetcrypto@gmail.com>
2023-12-28 00:17:12 +1300

    try

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,19 +1,14 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const commonConfiguration = require('./webpack.common.js');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge')
+const commonConfiguration = require('./webpack.common.js')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-// Production specific configurations
-const productionConfiguration = {
-    mode: 'production',
-    output: {
-        path: path.resolve(__dirname, '../dist'), // Output directory set to 'dist'
-        filename: '[name].[contenthash].js', // Naming convention for output files
-        publicPath: '/'
-    },
-    plugins: [
-        new CleanWebpackPlugin() // Plugin to clean the 'dist' folder before build
-    ]
-};
-
-module.exports = merge(commonConfiguration, productionConfiguration);
+module.exports = merge(
+    commonConfiguration,
+    {
+        mode: 'production',
+        plugins:
+        [
+            new CleanWebpackPlugin()
+        ]
+    }
+)
