bfe32328b8cbef1834ae6dee30b262ef99cf4ab3
kizmt <kizmetcrypto@gmail.com>
2024-01-21 21:43:41 +1300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import Products from './components/home/Products'
 import Steps from './components/home/Steps'
 import Api from './components/home/Api'
 import Faq from './components/home/Faq'
+import { GoogleAnalytics } from '@next/third-parties/google'
 export default function page() {
   return (
     <>
+    <GoogleAnalytics gaId="G-7E13GM6QB3" />
       <Hero />
       <Products />
       <Steps />
